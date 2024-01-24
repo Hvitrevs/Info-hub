e74acc3e0774adaf78f96c7519f7a682a39c3c37
Hvitrevs <venusof12pm@proton.me>
2024-01-24 15:00:16 -0500

    adding components

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <main>
-    space infohub
-    </main>
+  <main className="h full w full">
+    <div className="flex flex-col gap-20">
+      
+    </div>
+  </main>
   );
 }
