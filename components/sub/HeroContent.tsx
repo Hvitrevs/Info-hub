2babe0bfc42be2d16b4e9ae2194c06466d468521
Hvitrevs <venusof12pm@proton.me>
2024-01-27 14:25:09 -0500

    more mobile responsive layout

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -12,34 +12,34 @@ const HeroContent = () => {
     <motion.div
       initial="hidden"
       animate="visible"
-      className="flex flex-row items-center justify-center px-20 mt-20 w-full z-[20] xs:px-10"
+      className="flex flex-row items-center justify-center md:px-20 mt-20 w-full z-[20] xs:px-10"
     >
-      <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start xs:mt-20'>
+      <div className='md:h-full md:w-full flex flex-col gap-5 justify-center m-auto text-start xs:flex-nowrap'>
 
         {/* tagclouds */}
         
-        <div className='h-7 flex  gap-5 justify-start text-start xs:h-5'>
+        <div className='md:h-7 flex  md:gap-5 justify-start text-start  xs:flex-nowrap xs:gap-[5px] xs:h-5 xs:mb-[10px] xs:mt-20'>
         <motion.div
-          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9] "
         >
-          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5 xs:mr-[3px]" />
-          <h1 className="Welcome-text md:text-[13px] mr-1 xs:text-[7px] xs:text-nowrap">
+          <SparklesIcon className="text-[#b49bff] md:mr-[10px] md:h-5 md:w-5 xs:mr-[1px] xs:h-3 xs:w-3" />
+          <h1 className="Welcome-text md:text-[13px] mr-1 xs:text-[8px] xs:text-nowrap xs:mr-[1px]">
           Divine Agents Rulling
           </h1>
         </motion.div>
         <motion.div
           className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
         >
-          <TagIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text md:text-[13px] mr-1 xs:text-[7px]">
+          <TagIcon className="text-[#b49bff] md:mr-[10px] md:h-5 md:w-5 xs:mr-[1px] xs:h-3 xs:w-3" />
+          <h1 className="Welcome-text md:text-[13px] mr-1 xs:text-[8px] xs:text-nowrap xs:mr-[1px]">
           Tao
           </h1>
         </motion.div>
         <motion.div
           className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
         >
-          <StarIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text md:text-[13px] mr-1  xs:text-[7px] xs:text-nowrap">
+          <StarIcon className="text-[#b49bff] md:mr-[10px] md:h-5 md:w-5 xs:mr-[1px] xs:h-3 xs:w-3" />
+          <h1 className="Welcome-text md:text-[13px] mr-1 xs:text-[8px] xs:text-nowrap xs:mr-[1px]">
           Sun & Moon
           </h1>
         </motion.div>
@@ -50,7 +50,7 @@ const HeroContent = () => {
 
         <motion.div
           
-          className="flex flex-col gap-6 mt-6 text-5xl font-bold text-white max-w-[600px] w-auto h-auto md:text-[60px] xs:text-[25px] xs:mt-2"
+          className="flex flex-col gap-6 mt-6 text-5xl font-bold text-white max-w-[600px] w-auto h-auto md:text-[60px] xs:text-[35px]"
         >
           <span>
             Unravel
@@ -64,7 +64,7 @@ const HeroContent = () => {
 
         <motion.p
           
-          className="text-lg  text-gray-400 my-5 max-w-[600px] xs:text-sm xs:my-1 md:text-lg"
+          className="md:text-lg  text-gray-400 my-5 max-w-[600px] xs:text-sm xs:my-1 md:text-lg"
         > Learn how to use ancient knowledge and powers to find a way to better life, prosperity and happiness.
         </motion.p>
         <motion.a
@@ -77,9 +77,9 @@ const HeroContent = () => {
 
       <motion.div
       
-        className="w-full h-full flex justify-center items-center"
+        className="w-full h-full flex justify-center items-center xs:invisible md:visible"
       >
-        <Image className="filter saturate(200%)"
+        <Image className="filter saturate(200%) "
           src="/mainIconsdark.png "
           alt="icon"
           height={650}
