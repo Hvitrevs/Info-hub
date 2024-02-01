eea214cb6e282d7065d0abd443a1c6874170eac3
Hvitrevs <venusof12pm@proton.me>
2024-02-01 14:34:11 -0500

    building function

--- components/main/Stars.tsx
+++ components/main/Stars.tsx
@@ -1,9 +1,16 @@
 'use client'
 
-import React, { Suspense, useRef, useState } from "react"
-import {Canvas, useFrame } from '@react-three/fiber'
+import React, { useState, useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+// @ts-ignore
+import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = () => {
+  const ref: any = useRef();
+  const [sphere] = useState(() => 
+    random.inSphere(new Float32Array(5000), {radius: 1.2})
+  )
   return (
     <div>Stars</div>
   )
