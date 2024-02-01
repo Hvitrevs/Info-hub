d9f10f2da56ff8b4e3bf96cf1d42634b9f5a9361
Hvitrevs <venusof12pm@proton.me>
2024-02-01 14:29:32 -0500

    building 3D canvas from next.js

---
+++ components/main/Stars.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React, { Suspense, useRef, useState } from "react"
+import {Canvas, useFrame } from '@react-three/fiber'
+
+const Stars = () => {
+  return (
+    <div>Stars</div>
+  )
+}
+
+export default Stars
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -18,28 +18,28 @@ const HeroContent = () => {
 
         {/* tagclouds */}
         
-        <div className='md:h-7 flex  md:gap-5 justify-start text-start  xs:flex-nowrap xs:gap-[3px] xs:h-4  xs:mt-20 xxs:mt-40 xxs:h-4 xxs:gap-[2px]'>
+        <div className='md:h-6 flex  md:gap-5 justify-start text-start  xs:flex-nowrap xs:gap-[3px] xs:h-4  xs:mt-20 xxs:mt-40 xxs:h-4 xxs:gap-[2px]'>
         <motion.div
-          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9] "
+          className="Welcome-box py-[8px] px-[6px] border border-[#7042f88b] opacity-[0.9] "
         >
           <SparklesIcon className="text-[#b49bff] xs:py-[2px] sm:py-[2px] md:mr-[2px] md:h-4 md:w-4 xs:h-3 xs:w-3 xxs:w-2 xs:mr-[1px]" />
-          <h1 className="Welcome-text md:text-[13px] md:h-4 px-[5px] xs:text-[8px] xs:text-nowrap xxs:text-[5px] xxs:pr-0 border-l-[2px] border-[#7042f88b] opacity-[0.9]">
+          <h1 className="Welcome-text md:text-[12px] md:h-4 px-[5px] xs:text-[8px] xs:text-nowrap xxs:text-[5px] xxs:pr-0 border-l-[2px] border-[#7042f88b] opacity-[0.9]">
           Divine Agents Rulling
           </h1>
         </motion.div>
         <motion.div
-          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+          className="Welcome-box py-[8px] px-[6px] border border-[#7042f88b] opacity-[0.9]"
         >
           <TagIcon className="text-[#b49bff] xs:py-[3px] sm:py-[2px] md:mr-[2px] md:h-4 md:w-4 xs:h-3 xs:w-3 xxs:w-2" />
-          <h1 className="Welcome-text md:text-[13px] md:h-4 px-[5px] xs:text-[8px] xs:text-nowrap xxs:text-[5px] xxs:pr-0 border-l-[2px] border-[#7042f88b] opacity-[0.9]">
+          <h1 className="Welcome-text md:text-[12px] md:h-4 px-[5px] xs:text-[8px] xs:text-nowrap xxs:text-[5px] xxs:pr-0 border-l-[2px] border-[#7042f88b] opacity-[0.9]">
           Tao
           </h1>
         </motion.div>
         <motion.div
-          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+          className="Welcome-box py-[7px] px-[5px] border border-[#7042f88b] opacity-[0.9]"
         >
           <StarIcon className="text-[#b49bff] xs:py-[2px] sm:py-[2px] md:mr-[2px] md:h-4 md:w-4 xs:h-3 xs:w-3 xxs:w-2" />
-          <h1 className="Welcome-text md:text-[13px] md:h-4 px-[5px] xs:text-[8px] xs:text-nowrap xxs:text-[5px] xxs:pr-0 border-l-[2px] border-[#7042f88b] opacity-[0.9]">
+          <h1 className="Welcome-text md:text-[12px] md:h-4 px-[5px] xs:text-[8px] xs:text-nowrap xxs:text-[5px] xxs:pr-0 border-l-[2px] border-[#7042f88b] opacity-[0.9]">
           Sun & Moon
           </h1>
         </motion.div>
@@ -69,7 +69,7 @@ const HeroContent = () => {
         </motion.p>
         <motion.a
           
-          className="py-2 button-primary text-center text-white cursor-pointer rounded-2xl lg:text-[20px] md:w-[150px] xxs:w-[100px] xxs:text-[15px] p-2 hover:bg-[#4c2784]  animate-pulse"
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-md lg:text-[20px] md:w-[150px] xxs:w-[100px] xxs:text-[15px] p-2 hover:bg-[#4c2784]  animate-pulse"
         >
           Tell me!
         </motion.a>
